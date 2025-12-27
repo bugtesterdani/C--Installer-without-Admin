d9a0450a7b6874adf4fdd2486ac9280db61c8b5d
bugtesterdani <39159547+bugtesterdani@users.noreply.github.com>
2025-12-27 23:12:20 +0100

    Add using directives for Debugging and Reflection

--- Launcher_WPF/Updater.cs
+++ Launcher_WPF/Updater.cs
@@ -6,6 +6,8 @@ using System.Net.Http;
 using System.Text.Json;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Diagnostics;
+using System.Reflection;
 
 namespace Launcher_WPF
 {
